e8d79729db6fb9bdadaee5cd7afa3b0a02189338
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-02-14 00:50:27 +0530

    webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/**
+ * Created by jyothi on 8/1/17.
+ */
+
+/**
+ * Webpack config
+ */
+module.exports = {
+    entry: './App.js',
+
+    output: {
+        path:'./',
+        filename: 'index.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
